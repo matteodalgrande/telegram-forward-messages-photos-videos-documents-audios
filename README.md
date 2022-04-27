25daa9318ba2ce28e698c14b4638fd966d825bde
matteodalgrande <matteodalgrande97@gmail.com>
2022-04-27 09:19:17 +0200

    translation

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Script to forward all messages, videos, photoes, audios and documents from chat/
 This is a script that forward automatically all messages, videos, photoes, audios and documents from Telegram chat/group/channel/bot to another chat/group/channel/bot.
 It works 24/7 if you upload and run it in heroku or similar platform.
 
-In the first use you need to set the "config.ini" file with your settings.
-
 Have you any questions? Write to me in Twitter chat, my nickname is: @AKAmatteo
+
+---------
+
+### Installation
+
+`python3 -m pip install -r requirements.txt`
+
+In the first use you need to set the `config.ini` file with your settings.
--- script.py
+++ script.py
@@ -6,16 +6,16 @@ import json
 from telethon import TelegramClient, events
 from telethon.errors import SessionPasswordNeededError
 
-#per ottenere i membri del canale
+# to obtain channel's members
 import telethon.sync
 
 from pprint import pprint
 
-#Leggo il file config.ini
+# Read the config.ini file
 config = configparser.ConfigParser()
 config.read("config.ini")
 
-#Setto i valori di configurazione[che sono da inserire nel file config.ini]
+# Set the configuration value [which are in config.ini]
 api_id = config['Telegram']['api_id']
 api_hash = str(config['Telegram']['api_hash'])
 phone = config['Telegram']['phone']
@@ -24,16 +24,16 @@ username = config['Telegram']['username']
 chat_id = int(config['Telegram']['chat_id'])
 username_to_send = str(config['Telegram']['username_to_send'])
 
-#Creazione di un client e connessione allo stesso
+# Create a Client and connect to it
 client = TelegramClient(username, api_id, api_hash)
 @client.on(events.NewMessage)
 async def my_event_handler(event):
-    #pprint(dir(event))#dir serve per stampare l'oggetto con tutti parametri dell'evento
+    #pprint(dir(event))# dir  --> print the object whit all the parameters of the event
     if event.chat_id == chat_id:
         await client.send_message(username_to_send, event.message)
 
 client.start()
-#Questa riga serve per trovare l'id di una chat, tra virgolette si mette l'username
+# This line is useful to find out the id of a chat, in quotes you can put the username
 #pprint(dir(client.get_entity('Lucks123')))
 
 client.run_until_disconnected()
